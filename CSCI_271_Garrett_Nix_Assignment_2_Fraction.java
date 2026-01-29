0da8aebb1b19567b828b0529395f12826ead9ada
gnix22 <gjnix22@gmail.com>
2026-01-28 19:28:12 -0500

    tasks 1 and 2 success

--- CSCI_271_Garrett_Nix_Assignment_2_Fraction.java
+++ CSCI_271_Garrett_Nix_Assignment_2_Fraction.java
@@ -32,25 +32,46 @@ class Fraction{
         }
         return a;
     }
-    // My guess going based on what bits of the slides we have, we override toString which is what println calls. 
+    // My guess going based on what bits of the slides we have ( and errors given by implementation), we override toString which is what println calls. 
     // This outputs the result of everything.
     @Override
     public String toString(){
+        a_ /= gcf_;
+        b_ /= gcf_;
         String a = a_+""; // found you can coerce long to string
         String b = b_+"";
-        String resultString = a + "/" + b;
+        String resultString;
+        try{
+            // checks whether or not b_ = 1 or 0 by throwing exception if not.
+            long dividerCheck = a_/b_;
+            if(dividerCheck == a_){
+                resultString = a;
+            } else{
+                resultString = a + "/" + b;
+            }
+        } catch(ArithmeticException e){
+            // if a is pos, oo, if -a, -oo, a==0, NaN
+            if(a_ > 0){
+                resultString = "Infinity";
+            } else if(a_ < 0) {
+                resultString = "-Infinity";
+            } else {
+                resultString = "NaN";
+            }
+        }
         return resultString;
     }
     public Fraction(long a, long b){
         a_ = a;
         b_ = b;
         gcf_ = euclidGCD(a_, b_);
-        System.out.println("gcf is " + gcf_);
-        System.out.println("a is " + a_);
-        System.out.println("b is " + b_);
-        a_ /= gcf_;
-        b_ /= gcf_;
-        System.out.println("a is " + a_);
-        System.out.println("b is " + b_);
     }
+    public Fraction(long a){
+        a_ = a;
+        b_ = 1; // can eventually change just get rid of b_ = 1, but if need to call later keeping as placeholder.
+        gcf_ = euclidGCD(a_,b_);
+    }
+    //public Fraction divide(Fraction fraction){
+        
+    //}
 }
