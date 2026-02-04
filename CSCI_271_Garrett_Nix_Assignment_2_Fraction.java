94488f83965cc03f416a771d28a8fa4e6ab8f6e4
gnix22 <gjnix22@gmail.com>
2026-02-03 21:46:10 -0500

    work on testing, fixed pow function, code comments

--- CSCI_271_Garrett_Nix_Assignment_2_Fraction.java
+++ CSCI_271_Garrett_Nix_Assignment_2_Fraction.java
@@ -1,3 +1,29 @@
+/**************************************************************************
+* Project 2 for CSCI 271-001 Spring 2026
+* Author: Garrett Nix 
+* OS: NixOS 25.11 
+* Compiler: javac 25.0.1 
+* Date: Feb. 03, 2026 
+* Purpose
+* This program implements and tests a fraction class, creating ease of application
+* in future works.
+**************************************************************************/
+/********************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+* instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+* or any unauthorised sources, including the Internet, either
+* modified or unmodified.
+* - If any source code or documentation used in my program was
+* obtained from other sources, like a text book or course notes,
+* I have clearly indicated that with a proper citation in the
+* comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the supplied grading code.
+*
+* Garrett Nix 
+********************************************************************/
 class Fraction{
     private long a_;
     private long b_;
@@ -61,6 +87,20 @@ class Fraction{
     }
     // My guess going based on what bits of the slides we have ( and errors given by implementation), we override toString which is what println calls. 
     // This outputs the result of everything.
+    /******************************<toString>*****************************
+    * Description: Override of toString function that is typically called by println() 
+    *
+    * Parameters: none 
+    *
+    * Pre: system.out.println() needs to be called
+    *
+    * Post: this function will be called in place of the typical method String calls 
+    *
+    * Returns: string containing fraction string 
+    *
+    * Called by: main() 
+    * Calls:  
+    ************************************************************************/
     @Override
     public String toString(){
         a_ /= gcf_;
@@ -101,32 +141,63 @@ class Fraction{
     *
     * Returns: Fraction object consisting of resulting numerator and denominator
     *
-    * Called by: list function(s) that call this one 
-    * Calls: list function(s) that this one calls 
+    * Called by: main() 
+    * Calls: none 
     ************************************************************************/
     public Fraction multiply(Fraction fraction){
         // use convention of algebraic laws to show that 
         // (a/b)*(c/d) = ac / bd as new frac
-        long c = fraction.a_;
-        long d = fraction.b_;
-        long numerator = a_ * c;
-        long denominator = b_ * d;
+        long c = fraction.a_; // numerator of arg fraction
+        long d = fraction.b_; // denom of arg fraction
+        long numerator = a_ * c; // resulting numerator after multiplication
+        long denominator = b_ * d; // resulting denom after multiplication
         return new Fraction(numerator, denominator);
     }
+    /******************************<divide>*****************************
+    * Description: divides two fractions together. 
+    *
+    * Parameters: 
+    *   fraction: Fraction object consisting of fields within class.
+    *
+    * Pre: Two objects of type Fraction must be constructed in order to 
+    * perform the operation.
+    *
+    * Post: A new fraction object will be created based on the division
+    *
+    * Returns: Fraction object consisting of resulting numerator and denominator
+    *
+    * Called by: main() 
+    * Calls: none 
+    ************************************************************************/
     public Fraction divide(Fraction fraction){
         // use convention of algebraic laws to show that 
         // (a/b)/(c/d) = ad / bc as new frac
-        long c = fraction.a_;
-        long d = fraction.b_;
-        long numerator = a_ * d;
-        long denominator = b_ * c;
+        long c = fraction.a_; // numerator of arg fraction
+        long d = fraction.b_; // denom of arg fraction
+        long numerator = a_ * d; // resulting numerator
+        long denominator = b_ * c; // resulting denom
         return new Fraction(numerator, denominator);
     }
+    /******************************<add>*****************************
+    * Description: adds two fractions together 
+    *
+    * Parameters: 
+    *   fraction: a fraction to be passed in
+    *
+    * Pre: two fractions need to be constructed, and the method need to be called 
+    *
+    * Post: a new fraction will be obtained with the result of adding two fractions 
+    *
+    * Returns: a fraction object with the resulting numerator and denominator
+    *
+    * Called by: main() 
+    * Calls: none
+    ************************************************************************/
     public Fraction add(Fraction fraction){
-        long c = fraction.a_;
-        long d = fraction.b_;
+        long c = fraction.a_; // numerator of fraction being added
+        long d = fraction.b_; // denom of fraction being added
         long denominator = b_; // if b and d are equal, either can be chosen as denom as it doesn't matter 
-        long numerator = a_ + c;
+        long numerator = a_ + c; // simply add to find numerator if denom is same between frac
         // if denoms are diff, need to find gcd
         if(b_ != d){
             // for lcm, ensure pos vals
@@ -135,11 +206,26 @@ class Fraction{
             } else if(d < 0){
                 d *= -1;
             }
-            denominator = (b_ * d) / euclidGCD(b_,d); // just realized this needs to be lcm
+            denominator = (b_ * d) / euclidGCD(b_,d); // lcm calculation
             numerator = d * a_ + b_ * c;
         }
         return new Fraction(numerator, denominator);
     }
+    /******************************<subtract>*****************************
+    * Description: subtracts two fractions  
+    *
+    * Parameters: 
+    *   fraction: a fraction to be passed in
+    *
+    * Pre: two fractions need to be constructed, and the method need to be called 
+    *
+    * Post: a new fraction will be obtained with the result of subtracting two fractions 
+    *
+    * Returns: a fraction object with the resulting numerator and denominator
+    *
+    * Called by: main() 
+    * Calls: none
+    ************************************************************************/
     public Fraction subtract(Fraction fraction){
         long c = fraction.a_;
         long d = fraction.b_;
@@ -153,18 +239,52 @@ class Fraction{
             } else if(d < 0){
                 d *= -1;
             }
-            denominator = (b_ * d) / euclidGCD(b_,d); // just realized this needs to be lcm
+            denominator = (b_ * d) / euclidGCD(b_,d); // lcm calculation
             numerator = d * a_ - b_ * c;
         }
         return new Fraction(numerator, denominator);
     }
+    /******************************<pow>*****************************
+    * Description: Exponentiates a fraction
+    *
+    * Parameters:
+    *   int n - number of times to multiply fraction by itself
+    *
+    * Pre: a valid fraction object must be constructed in order to exponentiate it.
+    *
+    * Post: an exponentiated fraction will be obtained in the form of another fraction object.
+    *
+    * Returns: a new fraction object is returned with exponentiated values.
+    *
+    * Called by: main()
+    * Calls: none
+    ************************************************************************/
     public Fraction pow(int n){
-        for(int i=0; i < n; i++){
-            a_ *= a_;
-            b_ *= b_;
+        long a = a_; // temp values to exponentiate.
+        long b = b_; // same as a.
+        for(int i=0; i < n-1; i++){
+            a *= a_;
+            b *= b_;
+            System.out.println("a equals: " + a_);
+            System.out.println("b equals: " + b_);
         }
-        return new Fraction(a_, b_);
+        return new Fraction(a, b);
     }
+    /******************************<negate>*****************************
+    * Description: negates the value of a fraction 
+    *
+    * Parameters: none 
+    *
+    * Pre: a positive or negative fraction must be created and the method 
+    * negate must be called 
+    *
+    * Post: the negation of the fraction will be obtained
+    *
+    * Returns: the negation of the current fraction
+    *
+    * Called by: main()
+    * Calls: multiply
+    ************************************************************************/
     public Fraction negate(){
         return new Fraction(a_,b_).multiply(new Fraction(-1));
     }
--- CSCI_271_Garrett_Nix_Assignment_2_Main.java
+++ CSCI_271_Garrett_Nix_Assignment_2_Main.java
@@ -1,15 +1,53 @@
-/**
- * CSCI_271_Garrett_Nix_Assignment_2_Main
- */
+/**************************************************************************
+* Project 2 for CSCI 271-001 Spring 2026
+* Author: Garrett Nix 
+* OS: NixOS 25.11 
+* Compiler: javac 25.0.1 
+* Date: Feb. 03, 2026 
+* Purpose
+* This program implements and tests a fraction class, creating ease of application
+* in future works.
+**************************************************************************/
+/********************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+* instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+* or any unauthorised sources, including the Internet, either
+* modified or unmodified.
+* - If any source code or documentation used in my program was
+* obtained from other sources, like a text book or course notes,
+* I have clearly indicated that with a proper citation in the
+* comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the supplied grading code.
+*
+* Garrett Nix 
+********************************************************************/
 public class CSCI_271_Garrett_Nix_Assignment_2_Main {
+    /******************************<main>*****************************
+    * Description: Main function that tests fraction class 
+    *
+    * Parameters: args - cmd line arguments 
+    *
+    * Pre: successful compilation must happen before main runs 
+    *
+    * Post: tested results for fraction class will be obtained 
+    *
+    * Returns: none 
+    *
+    * Called by: none 
+    * Calls: add, multiply, divide, pow, negate
+    ************************************************************************/
     public static void main(String[] args) {
-        Fraction a = new Fraction(16);
+        Fraction a = new Fraction(16); // construct new fraction objects.
         Fraction d = new Fraction(0,0);
         Fraction b = new Fraction(3,5).add(new Fraction(7));
-        Fraction c = new Fraction(6,7); // 67!!!!!! (gonna kms).
-        //Fraction results = c.multiply(a.divide(b));
-        //System.out.println(results);
-        System.out.println(b);
+        Fraction c = new Fraction(6,7);
+        Fraction results = c.multiply(a.divide(b));
+        results = c.pow(3); // output should be 216/343.
+        results = c.pow(2).multiply(a.divide(b).add(a)).negate(); // fun function that should output something.
         System.out.println(d);
+        System.out.println(results);
     }
 }
